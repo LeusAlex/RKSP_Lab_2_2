c17fc9138b478ce58802c39de76e22f9ff2d8d4a
LeusAlex <sanya.leus@yandex.ru>
2025-12-16 19:28:35 +0300

    Шаги 1-6.

---
+++ src/main/java/ru/leus/gate/GateApplication.java
@@ -0,0 +1,15 @@
+package ru.leus.gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients(basePackages = "ru.leus.gate.client.api")
+public class GateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateApplication.class, args);
+	}
+
+}
