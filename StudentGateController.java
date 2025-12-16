dfdf257cea1554ff4a73593877ceb55320246d20
Alexander Leus <146023882+LeusAlex@users.noreply.github.com>
2025-12-16 22:35:26 +0300

    Add files via upload
    Добавил файл

---
+++ StudentGateController.java
@@ -0,0 +1,51 @@
+package ru.leus.gate;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import ru.leus.data.api.StudentGateApi;
+import ru.leus.data.model.StudentGateCreateRequest;
+import ru.leus.data.model.StudentGateResponse;
+import ru.leus.gate.client.api.StudentDataApi;
+import ru.leus.gate.client.model.StudentDataCreateRequest;
+import ru.leus.gate.client.model.StudentDataResponse;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentGateController implements StudentGateApi {
+
+    private final StudentDataApi studentsFeignClient;
+    /**
+     * Проксирует создание студента во внутренний DATA-SERVICE.
+     * Получает запрос от клиента, преобразует модель и перенаправляет.
+     */
+    @Override
+    public ResponseEntity<StudentGateResponse> createStudent(StudentGateCreateRequest request) {
+        StudentDataCreateRequest dataRequest = new StudentDataCreateRequest();
+        dataRequest.setFullName(request.getFullName());
+        dataRequest.setPassport(request.getPassport());
+
+        StudentDataResponse dataResponse = studentsFeignClient.createStudentDataInData(dataRequest);
+
+        StudentGateResponse gateResponse = new StudentGateResponse();
+        gateResponse.setId(dataResponse.getId());
+        gateResponse.setFullName(dataResponse.getFullName());
+        gateResponse.setPassport(dataResponse.getPassport());
+
+        return ResponseEntity.status(201).body(gateResponse);
+    }
+
+    @Override
+    public ResponseEntity<StudentGateResponse> getStudentById(Long studentId) {
+        // Вызываем Feign клиент для получения студента из data-service
+        StudentDataResponse dataResponse = studentsFeignClient.getStudentDataByIdFromData(studentId);
+
+        // Преобразуем ответ из data-service в формат gate-service
+        StudentGateResponse gateResponse = new StudentGateResponse();
+        gateResponse.setId(dataResponse.getId());
+        gateResponse.setFullName(dataResponse.getFullName());
+        gateResponse.setPassport(dataResponse.getPassport());
+
+        return ResponseEntity.ok(gateResponse);
+    }
+}
